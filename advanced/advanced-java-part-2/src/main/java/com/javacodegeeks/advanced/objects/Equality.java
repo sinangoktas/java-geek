463dd3b762cfa980cd98d47e1fd4482331569d78
sinangoktas <sinan.goktas@gmail.com>
2018-09-26 00:21:44 +0100

    initial commit

--- advanced/advanced-java-part-2/src/main/java/com/javacodegeeks/advanced/objects/Equality.java
+++ advanced/advanced-java-part-2/src/main/java/com/javacodegeeks/advanced/objects/Equality.java
@@ -3,7 +3,9 @@ package com.javacodegeeks.advanced.objects;
 public class Equality {
     public static void main(String[] args) {
         final String str1 = new String( "bbb" );
+        // checking if they are the very same objects
         System.out.println( "Using == operator: " + ( str1 == "bbb" ) );
+        // checking if they hold the same values
         System.out.println( "Using equals() method: " + str1.equals( "bbb" ) );
     }
 }
