652249f41f3ddb3b91939262e94743e6a9be9c7c
sinangoktas <sinan.goktas@gmail.com>
2018-10-06 18:28:00 +0100

    update

--- advanced/advanced-java-part-2/src/main/java/com/javacodegeeks/advanced/objects/OfficeToString.java
+++ advanced/advanced-java-part-2/src/main/java/com/javacodegeeks/advanced/objects/OfficeToString.java
@@ -3,7 +3,7 @@ package com.javacodegeeks.advanced.objects;
 public class OfficeToString {
     public static void main(String[] args) {
         final Person person = new Person( "John", "Smith", "john.smith@domain.com" );
-        final Office office = new Office();
+        final Office office = new Office( person );
         System.out.println( office.toString() );
     }
 }
