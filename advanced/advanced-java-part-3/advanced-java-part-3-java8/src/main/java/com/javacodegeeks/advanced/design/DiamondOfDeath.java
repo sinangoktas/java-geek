0f6a7e93501d5ef371ae47b20714ef71ce442b4c
Sinan Goktas <sinan.goktas@gmail.com>
2018-11-15 00:34:57 +0000

    update

--- advanced/advanced-java-part-3/advanced-java-part-3-java8/src/main/java/com/javacodegeeks/advanced/design/DiamondOfDeath.java
+++ advanced/advanced-java-part-3/advanced-java-part-3-java8/src/main/java/com/javacodegeeks/advanced/design/DiamondOfDeath.java
@@ -2,22 +2,22 @@ package com.javacodegeeks.advanced.design;
 
 public class DiamondOfDeath {
     interface A {
-        default void performAction() {            
+        default void performAction() {
         }
     }
-    
+
     interface B extends A {
         @Override
         default void performAction() {
         }
     }
-    
+
     interface C extends A {
         @Override
         default void performAction() {
         }
     }
-    
+
     // E is not compilable unless it overrides perfromAction() as well
     interface E extends B, C {
         @Override
@@ -25,4 +25,11 @@ public class DiamondOfDeath {
 
         }
     }
+
+    /*
+    java does not support multiple inheritance through class.
+    But through the interface, multiple inheritance is possible in java.
+    No java doesn't support multiple inheritance directly because it leads to overriding of methods
+    when both extended class have a same method nam
+     */
 }
